449ef1f8c724b25f1750fe456c8117862191cfd1
Tina Quach <quacht@mit.edu>
2017-04-28 17:29:26 -0400

    views in progress

---
+++ meh/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  meh
+//
+//  Created by Tina Quach on 4/28/17.
+//  Copyright © 2017 6.S062 Group. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Properties
+
+    @IBOutlet weak var username: UITextField!
+    
+    @IBAction func connectButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
