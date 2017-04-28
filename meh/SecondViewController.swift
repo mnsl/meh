fe65dd8d55bed7cf01b51af71dd25791eb294311
Jane Maunsell <maunsell@mit.edu>
2017-04-28 15:16:43 -0400

    Initial Commit

---
+++ meh/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  meh
+//
+//  Created by Jane Maunsell on 4/28/17.
+//  Copyright © 2017 6.S062 Group. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
